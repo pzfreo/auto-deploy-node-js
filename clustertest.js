aa07b0fb41095b7084e8d66f075cff2e2205866f
Paul Fremantle <pzfreo@github.com>
2015-11-16 19:16:28 +0000

    update

--- clustertest.js
+++ clustertest.js
@@ -7,7 +7,44 @@ var mysql     =    require('mysql');
 
 
 
+var app       =    express();
 
+var pool      =    mysql.createPool({
+	connectionLimit : 100, //important
+	host     : process.env.DBURL,
+	user     : 'root',
+	password : process.env.DBPW,
+	database : 'blah',
+	debug    :  false
+});
+
+function handle_database(req,res) {
+
+	pool.getConnection(function(err,connection){
+		if (err) {
+		  connection.release();
+		  res.json({"code" : 100, "status" : "Error in connection database"});
+		  return;
+		}   
+
+	
+		connection.query("select * from user",function(err,rows){
+			connection.release();
+			if(!err) {
+				res.json(rows);
+			}           
+		});
+
+		connection.on('error', function(err) {      
+			  res.json({"code" : 100, "status" : "Error in connection database"});
+			  return;     
+		});
+  });
+}
+
+app.get("/",function(req,res){
+	handle_database(req,res);
+});
 
 if (cluster.isMaster) {  
   // Master:
@@ -18,44 +55,7 @@ if (cluster.isMaster) {
   
 } else {
 
-	var app       =    express();
-
-	var pool      =    mysql.createPool({
-		connectionLimit : 100, //important
-		host     : process.env.DBURL,
-		user     : 'root',
-		password : process.env.DBPW,
-		database : 'blah',
-		debug    :  false
-	});
-
-	function handle_database(req,res) {
 	
-		pool.getConnection(function(err,connection){
-			if (err) {
-			  connection.release();
-			  res.json({"code" : 100, "status" : "Error in connection database"});
-			  return;
-			}   
-
-		
-			connection.query("select * from user",function(err,rows){
-				connection.release();
-				if(!err) {
-					res.json(rows);
-				}           
-			});
-
-			connection.on('error', function(err) {      
-				  res.json({"code" : 100, "status" : "Error in connection database"});
-				  return;     
-			});
-	  });
-	}
-
-	app.get("/",function(req,res){
-		handle_database(req,res);
-	});
 
 	app.listen(3001);
 
