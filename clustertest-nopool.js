8b65c9429c7fe51926d7b84d594899516fce88e0
Paul Fremantle <pzfreo@github.com>
2015-11-17 11:00:40 +0000

    switcheroo

---
+++ clustertest-nopool.js
@@ -0,0 +1,31 @@
+var express   =    require("express");
+var mysql     =    require('mysql');
+
+var app       =    express();
+
+
+
+app.get("/",function(req,res){
+  var connection = mysql.createConnection({
+	host     : process.env.DBURL,
+	user     : process.env.DBUSER,
+	password : process.env.DBPW,
+	database : 'blah'
+  });
+
+  connection.connect(function(err){
+  if(!err) {
+  } else {
+	  console.log("Error connecting database ... \n\n");  
+  }
+  });
+  connection.query('SELECT * from user order by age limit 5', function(err, rows, fields) {
+  connection.end();
+  if (!err)
+    res.json(rows);
+  else
+    console.log('Error while performing Query.');
+  });
+});
+
+app.listen(8080);
