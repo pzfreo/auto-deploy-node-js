723effa84cdf43c6e6333f89106cdc88c416234f
Paul Fremantle <pzfreo@github.com>
2015-11-17 10:35:45 +0000

    up

--- clustertest-old.js
+++ clustertest-old.js
@@ -7,48 +7,6 @@ var mysql     =    require('mysql');
 
 
 
-
-
-// app.listen(3000);
-// 
-// var pool      =    mysql.createPool({
-// 	connectionLimit : 100, //important
-// 	host     : process.env.DBURL,
-// 	user     : process.env.DBUSER,
-// 	password : process.env.DBPW,
-// 	database : 'blah',
-// 	debug    :  false
-// });
-// 
-// function handle_database(req,res) {
-// 
-// 	pool.getConnection(function(err,connection){
-// 		if (err) {
-// 		  connection.release();
-// 		  res.json({"code" : 100, "status" : "Error in connection database"});
-// 		  return;
-// 		}   
-// 
-// 	
-// 		connection.query("select * from user order by age",function(err,rows){
-// 			connection.release();
-// 			if(!err) {
-// 				res.json(rows);
-// 			}           
-// 		});
-// 
-// 		connection.on('error', function(err) {      
-// 			  res.json({"code" : 100, "status" : "Error in connection database"});
-// 			  return;     
-// 		});
-//   });
-// }
-// 
-// app.get("/",function(req,res){
-// 	handle_database(req,res);
-// });
-// 
-
 if (cluster.isMaster) {  
   // Master:
   // Let's fork as many workers as you have CPU cores
@@ -57,24 +15,25 @@ if (cluster.isMaster) {
   }
   
 } else {
-var app       =    express();
-
-var connection = mysql.createConnection({
-  host     : process.env.DBURL,
-  user     : process.env.DBUSER,
-  password : process.env.DBPW,
-  database : 'blah'
-});
+   var app       =    express();
 
 
-connection.connect(function(err){
-if(!err) {
-} else {
-    console.log("Error connecting database ... \n\n");  
-}
-});
+   app.get("/",function(req,res){
+	  var connection = mysql.createConnection({
+		host     : process.env.DBURL,
+		user     : process.env.DBUSER,
+		password : process.env.DBPW,
+		database : 'blah'
+	  });
 
-app.get("/",function(req,res){
+
+   connection.connect(function(err){
+   if(!err) {
+   } else {
+	   console.log("Error connecting database ... \n\n");  
+   }
+   });
+
   connection.query('SELECT * from user order by age limit 5', function(err, rows, fields) {
   connection.end();
   if (!err)
@@ -82,7 +41,7 @@ app.get("/",function(req,res){
   else
     console.log('Error while performing Query.');
   });
-});
+  });
 
 	app.listen(8080);
 }
