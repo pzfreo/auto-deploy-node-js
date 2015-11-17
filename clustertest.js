c59a0eba8c2610b38ae565a3e0e906614a713153
Paul Fremantle <pzfreo@github.com>
2015-11-17 09:40:43 +0000

    remove pool

--- clustertest.js
+++ clustertest.js
@@ -9,42 +9,70 @@ var mysql     =    require('mysql');
 
 var app       =    express();
 
-var pool      =    mysql.createPool({
-	connectionLimit : 100, //important
-	host     : process.env.DBURL,
-	user     : process.env.DBUSER,
-	password : process.env.DBPW,
-	database : 'blah',
-	debug    :  false
+var connection = mysql.createConnection({
+  host     : process.env.DBURL,
+  user     : process.env.DBUSER,
+  password : process.env.DBPW,
+  database : 'blah'
 });
 
-function handle_database(req,res) {
 
-	pool.getConnection(function(err,connection){
-		if (err) {
-		  connection.release();
-		  res.json({"code" : 100, "status" : "Error in connection database"});
-		  return;
-		}   
-
-	
-		connection.query("select * from user order by age",function(err,rows){
-			connection.release();
-			if(!err) {
-				res.json(rows);
-			}           
-		});
-
-		connection.on('error', function(err) {      
-			  res.json({"code" : 100, "status" : "Error in connection database"});
-			  return;     
-		});
-  });
+connection.connect(function(err){
+if(!err) {
+} else {
+    console.log("Error connecting database ... \n\n");  
 }
+});
 
 app.get("/",function(req,res){
-	handle_database(req,res);
+connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
+connection.end();
+  if (!err)
+    res.json(rows);
+  else
+    console.log('Error while performing Query.');
+  });
 });
+
+// app.listen(3000);
+// 
+// var pool      =    mysql.createPool({
+// 	connectionLimit : 100, //important
+// 	host     : process.env.DBURL,
+// 	user     : process.env.DBUSER,
+// 	password : process.env.DBPW,
+// 	database : 'blah',
+// 	debug    :  false
+// });
+// 
+// function handle_database(req,res) {
+// 
+// 	pool.getConnection(function(err,connection){
+// 		if (err) {
+// 		  connection.release();
+// 		  res.json({"code" : 100, "status" : "Error in connection database"});
+// 		  return;
+// 		}   
+// 
+// 	
+// 		connection.query("select * from user order by age",function(err,rows){
+// 			connection.release();
+// 			if(!err) {
+// 				res.json(rows);
+// 			}           
+// 		});
+// 
+// 		connection.on('error', function(err) {      
+// 			  res.json({"code" : 100, "status" : "Error in connection database"});
+// 			  return;     
+// 		});
+//   });
+// }
+// 
+// app.get("/",function(req,res){
+// 	handle_database(req,res);
+// });
+// 
 
 if (cluster.isMaster) {  
   // Master:
@@ -57,6 +85,6 @@ if (cluster.isMaster) {
 
 	
 
-	app.listen(3001);
+	app.listen(8080);
 
 }
