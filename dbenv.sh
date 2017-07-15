f02cda1a6686355f161ea903c048b7cad257ab52
Jang-Vijay Singh <jvsing@gmail.com>
2017-07-16 00:14:29 +0100

    Update dbenv.sh

--- dbenv.sh
+++ dbenv.sh
@@ -1,6 +1,6 @@
 # sets the database connection details from here so they can be altered on the fly without 
 # re-creating the autoscaling launch configuration
-export DBHOST=oxclodb-TEST.cluster-czz5taax8rqa.us-east-1.rds.amazonaws.com
+export DBHOST=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
 export DBPORT=3306
 
 #reminder: set DBUSER and DBPW in userdata
