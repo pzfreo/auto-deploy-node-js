71bd9cc5926578fcbe55eec76fbe47e6b312ae60
Jang-Vijay Singh <jvsing@gmail.com>
2017-07-17 19:31:48 +0100

    Added comments and DBURL environment variable

--- dbenv.sh
+++ dbenv.sh
@@ -1,6 +1,11 @@
 # sets the database connection details from here so they can be altered on the fly without 
-# re-creating the autoscaling launch configuration
-export DBHOST=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
-export DBPORT=3306
+#            re-creating the autoscaling launch configuration
+#            New instances created using the launch configuration will use this as long as 
+#            userdata sources this script
+DBURL=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
+
+#######The below options can be used as alternatives
+#export DBHOST=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
+#export DBPORT=3306
 
 #reminder: set DBUSER and DBPW in userdata
