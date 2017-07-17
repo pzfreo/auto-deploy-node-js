895309463129922b3ddf5ca70f99726d4f4df1ac
Jang-Vijay Singh <jvsing@gmail.com>
2017-07-17 19:45:37 +0100

    added export DBURL

--- dbenv.sh
+++ dbenv.sh
@@ -2,7 +2,7 @@
 #            re-creating the autoscaling launch configuration
 #            New instances created using the launch configuration will use this as long as 
 #            userdata sources this script
-DBURL=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
+export DBURL=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
 
 #######The below options can be used as alternatives
 #export DBHOST=oxclodb.czz5taax8rqa.us-east-1.rds.amazonaws.com
